4c2c928387c411f149c10755a143f470a1fd5863
Illuminary alpaca (Grant) <36704033+GrantGenesius@users.noreply.github.com>
2018-11-04 18:39:29 +0700

    added uncompleted purchase system on stage2 (CEO_computer)

---
+++ The_CEO_02/Assets/Scripts/purchasable_items.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class purchasable_items {
+
+    public string item_name;
+    public string item_description;
+    public string item_requirements;
+    public float item_cost;
+    public Sprite isPurchased;
+    public Sprite isNotPurchased;
+    public Sprite item_image;
+    public int item_availability;
+	
+}
---
+++ The_CEO_02/Assets/Scripts/purchasable_lists.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class purchasable_lists : MonoBehaviour {
+
+    public List<purchasable_items> item_list = new List<purchasable_items>();
+
+    public static purchasable_lists purchasable_lists;
+
+
+	// Use this for initialization
+	void Start () {
+        purchasable_lists = this;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void fill_list() { 
+        
+    }
+
+}
